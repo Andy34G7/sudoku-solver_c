2a960618f47b77929583996fb28615858477eadd
Anirudh <anirudhr1603@gmail.com>
2025-05-01 23:06:25 +0530

    added pos_safe function

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ bool pos_safe(int grid[N][N], int row, int col, int num);
 bool locate_empty(int grid[N][N], int *row, int *col);
 bool solve(int grid[N][N]);
 
+
 int main(){
     int grid[N][N];
     printf("--- Sudoku Solver ---\n");
@@ -22,15 +23,42 @@ int main(){
     printf("\nAttempting to Solve the puzzle...\n");
     //core logic here
 }
+
+
 void print_grid(int grid[N][N]){
     //print logic
 }
+
+
 bool pos_safe(int grid[N][N], int row, int col, int num){
     //check if the number is safe to place in the given position
+    bool flag = true; // setting flag to true as the default case
+    int r_start = row - (row % 3);
+    int c_start = col - (col % 3);
+    int rlim = row - (row % 3) + 3;
+    int clim = col - (col % 3) + 3;
+    for(int i = r_start; i < rlim; i ++){
+        for(int j = c_start; j < clim; j ++){
+            if (num == grid[i][j]){
+                flag = false;
+            }
+        }
+    }
+    for(int i = 0; i < N; i ++){
+        if (num == grid[i][col] || num == grid[row][i]){
+            flag = false;
+        }
+    }
+    return flag;
 }
+
+
 bool locate_empty(int grid[N][N], int *row, int *col){
     //find an empty position in the grid
 }
+
+
 bool solve(int grid[N][N]){
     //solve logic
 }
+

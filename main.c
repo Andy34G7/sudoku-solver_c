d31d19e64f18203359787d60dc4cebdcb4efa596
Anirudh <anirudhr1603@gmail.com>
2025-05-06 08:41:53 +0530

    Added print_grid function

--- main.c
+++ main.c
@@ -26,7 +26,12 @@ int main(){
 
 
 void print_grid(int grid[N][N]){
-    //print logic
+    for(int i = 0; i < N; i ++){
+        for(int j = 0; j < N; j ++){
+            printf("%d   ", grid[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 

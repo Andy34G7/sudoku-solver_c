4a4dce2c389995effb5fe805b9be28ee72028724
Anirudh <anirudhr1603@gmail.com>
2025-05-07 18:25:11 +0530

    cleaned everything up, added solve, and removed unnecessary functions

--- main.c
+++ main.c
@@ -7,21 +7,32 @@
 void print_grid(int grid[N][N]);
 bool pos_safe(int grid[N][N], int row, int col, int num);
 bool locate_filled(int grid[N][N], int *row, int *col);
-bool solve(int grid[N][N],int,int);
+bool solve(int grid[N][N]);
 
 
 int main(){
-    int grid[N][N];
+    int grid[N][N] = {
+        {5, 3, 0, 0, 7, 0, 0, 0, 0},
+        {6, 0, 0, 1, 9, 5, 0, 0, 0},
+        {0, 9, 8, 0, 0, 0, 0, 6, 0},
+        {8, 0, 0, 0, 6, 0, 0, 0, 3},
+        {4, 0, 0, 8, 0, 3, 0, 0, 1},
+        {7, 0, 0, 0, 2, 0, 0, 0, 6},
+        {0, 6, 0, 0, 0, 0, 2, 8, 0},
+        {0, 0, 0, 4, 1, 9, 0, 0, 5},
+        {0, 0, 0, 0, 8, 0, 0, 7, 9}
+    };
     printf("--- Sudoku Solver ---\n");
-    printf("Enter the Sudoku grid (0 for empty cells):, row by row, separated by a space.\n");
-    for (int i = 0; i < N; i++) {
-        printf("Row %d: \nElements:-\n1 2 3 4 5 6 7 8 9\n", i + 1);
-        scanf("%d %d %d %d %d %d %d %d %d", &grid[i][0], &grid[i][1], &grid[i][2], &grid[i][3], &grid[i][4], &grid[i][5], &grid[i][6], &grid[i][7], &grid[i][8]);
-    }
+    
     printf("The Sudoku grid you entered is:\n");
     print_grid(grid);
     printf("\nAttempting to Solve the puzzle...\n");
-    //core logic here
+    if(solve(grid)){
+        print_grid(grid);
+    }
+    else{
+        printf("There is no solution to the puzzle\n"); 
+    }
 }
 
 
@@ -42,6 +53,7 @@ bool pos_safe(int grid[N][N], int row, int col, int num){
     int c_start = col - (col % 3);
     int rlim = row - (row % 3) + 3;
     int clim = col - (col % 3) + 3;
+    // Checking the sub grid
     for(int i = r_start; i < rlim; i ++){
         for(int j = c_start; j < clim; j ++){
             if (num == grid[i][j]){
@@ -49,6 +61,7 @@ bool pos_safe(int grid[N][N], int row, int col, int num){
             }
         }
     }
+    // Checking rows and columns simultaneously
     for(int i = 0; i < N; i ++){
         if (num == grid[i][col] || num == grid[row][i]){
             flag = false;
@@ -58,27 +71,26 @@ bool pos_safe(int grid[N][N], int row, int col, int num){
 }
 
 
-bool locate_filled(int grid[N][N], int *row, int *col){
-    //find a filled position in the grid
-    for (*row = 0; *row < N; (*row)++) {
-        for (*col = 0; *col < N; (*col)++) {
-            if (grid[*row][*col] == 0) {
-                return false;
+
+bool solve(int grid[N][N]){
+    for(int i = 0; i < N; i ++){
+        for (int j = 0; j < N; j++){
+            if (grid[i][j] == 0){ // Checking whether the position is empty
+                for (int num = 1; num < 10; num ++){
+                    if (pos_safe(grid, i, j, num))// Checking if the position is safe
+                    {
+                        grid[i][j] = num; // Placing the number here and attempting to solve the grid
+                        if (solve(grid)){
+                            return true; // If grid is solved 
+                        }
+                        
+                        grid[i][j] = 0; // Otherwise reset that position back to 0, and attempt to solve the sudoku once again
+                    }
+                }
+                return false; 
             }
+            
         }
     }
-    return true;
-}//instead of locating empty, i located the filled, and when filled cell is found, idea is to just go to next cell
-
-
-bool solve(int grid[N][N],int row, int col){
-    //solve logic
-    if (row == 9)//solving row after row, rows--> 0 to 8, i.e. 9 means out of bounds
-    print_grid(grid);
-
-    if (col == 9) 
-    return recurr(grid, row + 1, 0);
-    
-    if (locate_filled(grid,&row,&col))
-    return recurr(grid, row, col + 1);//i.e. since it is filled, you skip this cell and go to the next one
+    return true; // In case the sudoku provided is already solved
 }
